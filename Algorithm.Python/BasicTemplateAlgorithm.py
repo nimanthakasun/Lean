cd48687e44fb5d7368a4d1f545bec6ed40609545
Dhananjani <38600977+Dhananjani@users.noreply.github.com>
2018-05-11 14:36:50 +0530

    hghgvh

--- Algorithm.Python/BasicTemplateAlgorithm.py
+++ Algorithm.Python/BasicTemplateAlgorithm.py
@@ -11,6 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#dlhdjhfwuwefu
+
 from clr import AddReference
 AddReference("System")
 AddReference("QuantConnect.Algorithm")
